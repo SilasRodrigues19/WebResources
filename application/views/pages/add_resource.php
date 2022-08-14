3ec7299eb806703adfe1dfaabd01d6e3e08a3a00
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-08-14 18:35:25 -0300

    feat: update background image and select style

--- application/views/pages/add_resource.php
+++ application/views/pages/add_resource.php
@@ -41,7 +41,7 @@
 
           <div class="form-group mt-4">
             <label for="resource_category">ID da categoria</label>
-            <select name="resource_category" id="resource_category"  class="form-control">
+            <select name="resource_category" id="resource_category" class="form-select">
                 <option value="" selected disabled>Selecione a categoria</option>
                 <?php foreach($showCategoryOption as $idx => $value): ?>
                     <option value="<?= $showCategoryOption[$idx]['category_id']; ?>">

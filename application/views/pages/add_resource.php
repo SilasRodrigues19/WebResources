9144ab5d6dac95223fe3e6f061aedfadff2a3458
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-08-14 21:23:08 -0300

    refactor: remove text animation

--- application/views/pages/add_resource.php
+++ application/views/pages/add_resource.php
@@ -13,7 +13,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<h3 class="text-center title">Adicionar recurso</h3>
+				<h3 class="text-center">Adicionar recurso</h3>
         <?= showMessage(); ?>
 			</div>
 		</div>

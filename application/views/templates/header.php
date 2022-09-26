37ef65d1c37917ef0a80b3eea58165a940202e81
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-26 12:36:39 -0300

    feat: update logo

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,8 +8,7 @@
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=B612+Mono:wght@400;700&display=swap" rel="stylesheet">
 		<link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('apple-touch-icon.png'); ?>">
-		<link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('favicon-32x32.png'); ?>">
-		<link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('favicon-16x16.png'); ?>">
+		<link rel="icon" type="image/svg+xml" href="<?= base_url('logo.svg'); ?>" />
 		<link rel="manifest" href="<?= base_url('site.webmanifest'); ?>">
 		<title><?= $title; ?></title>
 		<link rel="stylesheet" href="<?= base_url('public/libraries/sweetalert2/sweetalert2.min.css'); ?>">

e167daa6520dcd25dafb5198d67e58119ffd04e6
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-11-10 15:21:41 -0300

    refactor: add meta tags

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		 
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+		<meta name="theme-color" content="#6a85b6">
+		<meta name="author" content="Silas Rodrigues">
+		<meta name="description" content="Coleção de links com recursos para desenvolvimento web, separados por categorias" />
+		<meta name="keywords" content="recursos, frontend, links, utilitários, inspiração, ilustrações, ícones, ui material, referências, paleta de cores, gradientes, templates, hospedagens, snippets">
+		<meta property="og:locale" content="pt_BR" />
+		<meta property="og:title" content="Recursos para desenvolvimento web" />
+		<meta property="og:description" content="Coleção de links com recursos para desenvolvimento web, separados por categorias" />
+		<meta property="og:url" content="https://rockcontent.com/br/talent-blog/ferramentas-de-front-end/" />
+		<meta property="og:site_name" content="WebResources" />
+		<meta property="og:type" content="article" />
+		<meta property="og:image" content="https://github.com/SilasRodrigues19/WebResources/raw/main/public/img/screenshots/preview.png" />
+		<meta property="og:image:width" content="1366" />
+		<meta property="og:image:height" content="2189" />
+		<meta property="og:image:type" content="image/png" />
+		<meta name="twitter:creator" content="@Jinuye1" />
+		<meta name="twitter:title" content="Recursos para desenvolvimento web" />
+		<meta name="twitter:description" content="Coleção de links com recursos para desenvolvimento web, separados por categorias" />
+
 		<link href="https://fonts.googleapis.com/css2?family=B612+Mono:wght@400;700&display=swap" rel="stylesheet">
 		<link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('apple-touch-icon.png'); ?>">
 		<link rel="icon" type="image/svg+xml" href="<?= base_url('logo.svg'); ?>" />
@@ -16,6 +35,7 @@
 		<link rel="stylesheet" href="<?= base_url('public/libraries/animate/animate.min.css'); ?>">
 		<link rel="stylesheet" href="<?= base_url('public/libraries/select2/select2.min.css'); ?>">
 		<link rel="stylesheet" href="<?= base_url('public/css/style.css'); ?>">
+		<link rel="canonical" href="<?= base_url(); ?>">
 	</head>
 
 	<body>
